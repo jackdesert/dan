787307ed74a8efcea4b039ac221d5fc87c72bc16
Jack Desert <jdtzn@riseup.net>
2018-06-27 22:07:25 -0500

    challenge1

---
+++ 01_add_me.js
@@ -0,0 +1,7 @@
+var ages = [1, 3, 72, 52, 60, 42, 50]
+
+// 1. Calculate the mean age and print it
+
+
+
+// 2. Calculate the max age and print it
---
+++ 01_add_me.py
@@ -0,0 +1,9 @@
+#! /usr/env/python
+
+ages = [1, 3, 72, 52, 60, 42, 50]
+
+# 1. Calculate the mean age and print it
+
+
+
+# 2. Calculate the max age and print it
