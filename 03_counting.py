5bae3a828df4a208397736a4f562a2dbd75f787b
Jack Desert <jdtzn@riseup.net>
2018-07-02 17:02:05 -0500

    added counting

---
+++ 03_counting.py
@@ -0,0 +1,27 @@
+# Counting in Python
+#
+# 1. Read https://www.programiz.com/python-programming/methods/built-in/range
+# 2. Count from 1 to 100
+# 3. Count from 2 to 44 in increments of 7
+# 4. Count from 1600 down to 137 in increments of 11
+#
+# 5. Print the first 98 verses of "99 Bottles of Beer on the Wall".
+#     Here are the first two verses so you can see the patern:
+#
+#     99 bottles of beer on the wall
+#     99 bottles of beer
+#     Take one down, pass it around
+#     98 bottles of beer on the wall!
+#
+#     98 bottles of beer on the wall
+#     98 bottles of beer
+#     Take one down, pass it around
+#     97 bottles of beer on the wall!
+
+
+# Javascript
+#
+# 1. Read https://www.w3schools.com/js/js_loop_while.asp
+# 2-5 Same as Python 2-5
+
+
