015bef5b17690919d3c16f94708e5443a3b65439
Jack Desert <jdtzn@riseup.net>
2018-07-02 16:56:00 -0500

    Array Indexing

---
+++ 05_array_indexing.py
@@ -0,0 +1,20 @@
+# Array indexing in Python
+# 1. Read https://www.programiz.com/python-programming/array
+#    Note they call it an array, but in Python it's called a list
+
+# 2. The following arrays represent how many Euros Sam earned during a three-week period:
+#         Sun Mon Tue Wed Thu Fri Sat
+week_1 = [ 23, 15,  2, 73, 40, 10, 3]
+week_2 = [ 10,  5,  6, 27, 15,  3, 9]
+week_3 = [ 70,  2, 15,  3, 27,  8, 5]
+
+# 3. How much money was earned during week 1?
+# 4. What was the total amount earned on Sundays?
+# 5. What was the total amount earned on Tuesdays?
+# 6. On which day of the week, on average, was the most money earned?
+# 7. On which day of the week, on average, was the least money earned?
+# 8. What was the total amount earned on Sundays?
+# 9. What was the total amount earned on Tuesdays?
+# 10. What was the total amount earned Mon - Thurs during week 1?
+# 11. What was the total amount earned Tue - Fri during weeks 2 and 3?
+# 12. Repeat 3-11 in JavaScript
